65968f4e9b867b1c7a70023eede155a502315ca0
Sam Mulqueen <sam.mulqueen.nz@gmail.com>
2023-03-12 14:19:37 +1300

    Fixed once more

--- www/web-app.js
+++ www/web-app.js
@@ -184,12 +184,12 @@ async function getNonce(address) {
 async function mintStandard(){
     if(CONTRACT.mintPaused) return alert('Minting is currently paused.');
     if(!await ethEnabled() && ACCOUNT) return alert('You must be connected with MetaMask to proceed.')
-    if(await ethersProvider.getNetwork() !== chainId) return alert("Please switch to your MetaMask to use the Polygon Network and try again.")
+    if(await ethersProvider.getNetwork().chainId !== chainId) return alert("Please switch to your MetaMask to use the Polygon Network and try again.")
 
     let quantity = parseInt(document.getElementById('standard_quantity').value);
     if(quantity<1) return alert('Quantity must be atleast 1');
     if(quantity>CONTRACT.maxQuantity) return alert('Quantity must be '+CONTRACT.maxQuantity+' or less');
-    
+
     try{
         let gasFee = await getGasPrice();
         let nonce = await getNonce(ACCOUNT);
@@ -209,7 +209,7 @@ async function mintStandard(){
 async function mintCustom(){
     if(CONTRACT.mintPaused) return alert('Minting is currently paused.');
     if(!await ethEnabled() && ACCOUNT) return alert('You must be connected with MetaMask to proceed.')
-    if(await ethersProvider.getNetwork() !== chainId) return alert("Please switch to your MetaMask to use the Polygon Network and try again.")
+    if(await ethersProvider.getNetwork().chainId !== chainId) return alert("Please switch to your MetaMask to use the Polygon Network and try again.")
 
     let quantity = parseInt(document.getElementById('custom_quantity').value);
     if(quantity<1) return alert('Quantity must be atleast 1');
@@ -308,8 +308,7 @@ function handleTransactionResponse(txn){
 
 const ethEnabled = async () => {
     if (window.ethereum) {
-        ethersProvider = new ethers.providers.Web3Provider(window.ethereum);
-        ethersSigner = ethersProvider.getSigner();
+        await chainSwitchInstall(chainId);
 
         let accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
         ACCOUNT = accounts[0]
@@ -321,9 +320,10 @@ const ethEnabled = async () => {
 
         if(ACCOUNT) onConnect();
 
-        await chainSwitchInstall(chainId);
+        ethersProvider = new ethers.providers.Web3Provider(window.ethereum);
+        ethersSigner = ethersProvider.getSigner();
 
-        if(await ethersProvider.getNetwork() == chainId) FortuneCookieWrite = new ethers.Contract(FortuneCookie_address, FortuneCookie_ABI, ethersSigner);
+        if(await ethersProvider.getNetwork().chainId == chainId) FortuneCookieWrite = new ethers.Contract(FortuneCookie_address, FortuneCookie_ABI, ethersSigner);
 
         return true;  
     }  
